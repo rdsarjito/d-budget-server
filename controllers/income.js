4dd5825c8c41be5ff4c82b44882eacb6d1507bee
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-08 10:31:27 +0700

    [UPDATE]UserId

--- controllers/income.js
+++ controllers/income.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Income = mongoose.model('incomes');
 
 const createIncome = async(req, res) => {
+  const userId = req.user.userData._id;
   const { description, amount, category } = req.body.data;
   const date = new Date();
   const typeBalance = 'income';
-  const newIncome = new Income ({ description, amount, category, date, typeBalance });
+  const newIncome = new Income ({ userId, description, amount, category, date, typeBalance });
 
   const income = await newIncome.save();
 
@@ -22,7 +23,7 @@ const getIncome = async(req, res) => {
 };
 
 const deleteIncome = async(req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
   const income = await Income.findByIdAndDelete(id);
   res.send(income)
 }
--- routes/income.js
+++ routes/income.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const accesToken = require('../middlewares/accesToken');
 const incomeControllers = require('../controllers/income');
 
-router.post('/', incomeControllers.createIncome);
-router.get('/', incomeControllers.getIncome);
-router.delete('/:id', incomeControllers.deleteIncome);
+router.post('/', accesToken, incomeControllers.createIncome);
+router.get('/', accesToken, incomeControllers.getIncome);
+router.delete('/:id', accesToken, incomeControllers.deleteIncome);
 
 module.exports = router;
