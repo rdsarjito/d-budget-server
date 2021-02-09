c0dcdefc808e01905cc8b988262a6b6a01fefc72
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-02-09 20:58:52 +0700

    [ADD]JsonWebToken

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv').config({ path: './.env' }).parsed;
 
 module.exports = {
+  accesTokenSecret: dotenv.ACCES_TOKEN_SECRET,
   mongoURI: dotenv.MONGO_URI,
   googleClientID: dotenv.GOOGLE_CLIENT_ID,
-  googleClientSecret: dotenv.GOOGLE_CLIENT_SECRET,
-  accesTokenSecret: dotenv.ACCES_TOKEN_SECRET
+  googleClientSecret: dotenv.GOOGLE_CLIENT_SECRET
 };
